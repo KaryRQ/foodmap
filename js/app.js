6564c2d2c95fd196ab827dfe50e0770312fff70d
KaryRQ <darkness.kary@hotmail.com>
2018-01-22 18:38:33 -0600

    función con modal en comidas

--- js/app.js
+++ js/app.js
@@ -4,27 +4,34 @@ $().ready(function () {
     $("#splash").fadeIn(0000).fadeOut(2000);
 });
 
-$(document).ready(function () {
-    setTimeout(function () {
+$(document).ready(function() {   
+    setTimeout(function() {
         $("#container2").fadeIn(1500);
-    }, 2000);
+    },2000);
 });
 
-/*busqueda por tipo de comida */
-$(document).ready(function () {
-    $('#buscador').keyup(function () {
-        var tiposComida = $('.tiposComida');
-        var buscar = $(this).val();
-        var comida = '';
-        for (var i = 0; i < tiposComida.length; i++) {
-            comida = $(tiposComida[i]).html().toLowerCase();
-            for (var x = 0; x < comida.length; x++) {
-                if (buscar.length == 0 || comida.indexOf(buscar) > -1) {
-                    $(tiposComida[i]).parents('.comida').show();
-                } else {
-                    $(tiposComida[i]).parents('.comida').hide();
+
+
+$(document).ready(function(){
+    $('#buscador').keyup(function(){
+       var tipo = $('.tipo');
+       var buscar = $(this).val();
+       var comida='';
+       for( var i = 0; i < tipo.length; i++ ){
+           comida = $(tipo[i]).html().toLowerCase();
+            for(var x = 0; x < comida.length; x++ ){
+                if( buscar.length == 0 || comida.indexOf( buscar ) > -1 ){
+                    $(tipo[i]).parents('.comida').show(); 
+                }else{
+                     $(tipo[i]).parents('.comida').hide();
                 }
             }
-        }
+       }
     });
-});
+  });
+
+  $(document).ready(function() {
+    $('#cerrar').click(function() {
+      $('#buscador').val('');
+    });
+  });
